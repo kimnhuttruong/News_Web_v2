8e72385a07c75ba31608df4d08b4ed9cf329b5e3
Truong Kim <star.knt.8@gmail.com>
2020-08-11 21:44:24 +0700

    xx

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ app.use(function (err, req, res, next) {
 })
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, function () {
+  console.log(PORT);
   console.log(`Server is running at http://localhost:${PORT}`);
 })
